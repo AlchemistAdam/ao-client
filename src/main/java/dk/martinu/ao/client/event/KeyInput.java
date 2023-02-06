20538817d4dcd4c477d0676e42989c522952e40c
Adam Martinu <1337programmering.c@gmail.com>
2023-02-06 18:22:20 +0100

    Added KeyInput

---
+++ src/main/java/dk/martinu/ao/client/event/KeyInput.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2023, Adam Martinu. All rights reserved. Altering or
+ * removing copyright notices or this file header is not allowed.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package dk.martinu.ao.client.event;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.event.KeyEvent;
+import java.util.Objects;
+
+import dk.martinu.ao.client.*;
+
+/**
+ * Container class used by {@link AbstractTarget} objects when moving key
+ * events from the Event Dispatch Thread (EDT), used by the AWT, to a target's
+ * {@link KeyInputBuffer}. {@code KeyInput} objects store a key event that
+ * occurred on the EDT and an {@link KeyAction action} that will be performed
+ * when the input is {@link #process() processed}.
+ *
+ * @author Adam Martinu
+ * @since 1.0
+ * @version 1.0, 2023-06-02
+ */
+public record KeyInput(@NotNull KeyAction action, @NotNull KeyEvent event) {
+
+    /**
+     * Constructs a new {@code KeyInput} object with the specified key action
+     * and key event.
+     *
+     * @param action The key action
+     * @param event  The key event
+     * @throws NullPointerException if {@code action} or {@code event} is
+     * {@code null}
+     */
+    public KeyInput(@NotNull final KeyAction action, @NotNull final KeyEvent event) {
+        this.action = Objects.requireNonNull(action, "action must not be null");
+        this.event = Objects.requireNonNull(event, "event is null");
+    }
+
+    /**
+     * Performs the key action and returns the boolean value returned by the
+     * call.
+     *
+     * @return The boolean value returned by the key action call.
+     * @see KeyAction
+     */
+    public boolean process() {
+        return action.perform(event);
+    }
+}
