08b3d41657cbd0029287b94cfcb465a627c8c3db
Adam Martinu <1337programmering.c@gmail.com>
2023-02-06 20:39:37 +0100

    Updated license header

--- src/main/java/dk/martinu/ao/client/core/Launcher.java
+++ src/main/java/dk/martinu/ao/client/core/Launcher.java
@@ -1,10 +1,18 @@
 /*
- * Copyright (c) 2018, Adam Martinu. All rights reserved. Altering or removing
- * copyright notices or this file header is not allowed.
+ * Copyright (c) 2023, Adam Martinu. All rights reserved. Altering or
+ * removing copyright notices or this file header is not allowed.
  *
- * This code is licensed under the Creative Commons Attribution-ShareAlike 4.0
- * International License (CC BY-SA 4.0). To view a copy of the license, visit
- * https://creativecommons.org/licenses/by-sa/4.0/legalcode.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
 package dk.martinu.ao.client.core;
 
@@ -148,9 +156,9 @@ class Launcher {
      * {@link #thread} is started.
      *
      * @param target The primary target
-     * @throws NullPointerException if {@code target} is {@code null}
+     * @throws NullPointerException  if {@code target} is {@code null}
      * @throws IllegalStateException if this launcher's thread has already been
-     * started
+     *                               started
      */
     final void run(@NotNull final Target target) throws IllegalStateException {
         Objects.requireNonNull(target, "target is null");
