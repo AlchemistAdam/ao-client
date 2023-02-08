1f3c3b4f202587f60db333a5a844f92ace7aa252
Adam Martinu <1337programmering.c@gmail.com>
2023-02-08 17:46:44 +0100

    Updated license header

--- src/main/java/dk/martinu/ao/client/targets/Intro.java
+++ src/main/java/dk/martinu/ao/client/targets/Intro.java
@@ -1,10 +1,18 @@
 /*
- * Copyright (c) 2020, Adam Martinu. All rights reserved. Altering or removing
- * copyright notices or this file header is not allowed.
+ * Copyright (c) 2023, Adam Martinu. All rights reserved. Altering or
+ * removing copyright notices or this file header is not allowed.
  *
- * This code is licensed under the Creative Commons Attribution-ShareAlike 4.0
- * International License (CC BY-SA 4.0). To view a copy of the license, visit
- * https://creativecommons.org/licenses/by-sa/4.0/legalcode.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
 package dk.martinu.ao.client.targets;
 
