dcebd101dc58d90808a05a3e97972acd83fdb80c
Adam Martinu <1337programmering.c@gmail.com>
2023-06-16 14:38:59 +0200

    getComponents() method now returns an unmodifiable list

--- src/main/java/dk/martinu/ao/client/ui/Container.java
+++ src/main/java/dk/martinu/ao/client/ui/Container.java
@@ -93,7 +93,7 @@ public class Container extends Component implements Iterable<Component> {
     @Contract(pure = true)
     @NotNull
     public List<Component> getComponents() {
-        return components;
+        return Collections.unmodifiableList(components);
     }
 
     @Contract(pure = true)
