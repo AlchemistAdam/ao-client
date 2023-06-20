c80785ea89d2521e454ff9882ca0fac71f16e5c6
Adam Martinu <1337programmering.c@gmail.com>
2023-06-20 21:25:20 +0200

    Added method to read Theme objects from a KoFi document file

--- src/main/java/dk/martinu/ao/client/ui/Theme.java
+++ src/main/java/dk/martinu/ao/client/ui/Theme.java
@@ -18,9 +18,13 @@ package dk.martinu.ao.client.ui;
 
 import org.jetbrains.annotations.*;
 
-import java.util.Objects;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.*;
 
 import dk.martinu.ao.client.util.SimpleHashMap;
+import dk.martinu.kofi.*;
+import dk.martinu.kofi.codecs.KofiCodec;
 
 /**
  * A theme containing styles mapped to identifiers.
@@ -32,6 +36,114 @@ import dk.martinu.ao.client.util.SimpleHashMap;
  * @since 1.0
  */
 public class Theme {
+
+    /**
+     * Reads a theme from the KoFi {@link Document} file at the
+     * specified path and returns it.
+     * DOC syntax of theme document
+     *
+     * @param path the path to read from
+     * @return a theme
+     * @throws IOException if an error occurred when reading from the path
+     * @throws UIException if an error occurred when converting the contents of
+     *                     the file to a theme
+     */
+    @Contract(value = "_ -> new", pure = true)
+    public static Theme readFrom(@NotNull final Path path) throws IOException, UIException {
+        // get theme document
+        final Document doc = KofiCodec.provider().readFile(path);
+        // namespace used to locate classes for class styles
+        String namespace = doc.getString(null, "namespace", "");
+        if (!namespace.isEmpty())
+            namespace += '.';
+        // style maps
+        final SimpleHashMap<String, Style> nameStyles = new SimpleHashMap<>();
+        final SimpleHashMap<Class<? extends Component>, Style> classStyles = new SimpleHashMap<>();
+        // list of style-parent pairs that need to be resolved
+        final ArrayList<Object> resolve = new ArrayList<>();
+        // create styles and populate maps
+        for (Section section : doc.getSections()) {
+            final List<Property<?>> propertyList = doc.getProperties(section.name);
+            // filter out style properties before style is constructed
+            final ArrayList<Property<?>> styleProperties = new ArrayList<>();
+            for (int i = propertyList.size() - 1; i >= 0; i--) {
+                if (propertyList.get(i).key.startsWith("$"))
+                    styleProperties.add(propertyList.remove(i));
+                ;
+            }
+            // create style
+            final Style style;
+            try {
+                style = new Style(propertyList);
+            }
+            catch (Exception e) {
+                throw new UIException("could not create style", e);
+            }
+            // name style
+            if (section.name.startsWith("@")) {
+                nameStyles.put(section.name.substring(1), style);
+            }
+            // class style
+            else {
+                final String className = namespace + section.name;
+                try {
+                    final Class<?> cls = Class.forName(className);
+                    if (Component.class.isAssignableFrom(cls)) {
+                        //noinspection unchecked
+                        classStyles.put((Class<? extends Component>) cls, style);
+                    }
+                    else
+                        throw new UIException("class style {" + className + "} is not assignable to Component");
+                }
+                catch (ClassNotFoundException e) {
+                    throw new UIException("cannot find class for class style {" + className + "}", e);
+                }
+            }
+            // style properties
+            for (Property<?> p : styleProperties) {
+                if (p.matches("$parent")) {
+                    resolve.add(style);
+                    resolve.add(String.valueOf(p.value));
+                }
+            }
+        }
+        // resolve parent styles
+        for (int i = 0; i < resolve.size(); i += 2) {
+            final Style style = (Style) resolve.get(i);
+            final String parentName = (String) resolve.get(i + 1);
+            // name style parent
+            if (parentName.startsWith("@")) {
+                final Style nameStyle = nameStyles.get(parentName.substring(1));
+                if (nameStyle != null)
+                    style.setParent(nameStyle);
+                else
+                    throw new UIException("could not resolve parent name style {" + parentName + "}");
+            }
+            // class style parent
+            else {
+                final String className = namespace + parentName;
+                try {
+                    // runtime type of the component the parent style applies to
+                    final Class<?> cls = Class.forName(className);
+                    if (Component.class.isAssignableFrom(cls)) {
+                        //noinspection unchecked
+                        final Style classStyle = classStyles.get((Class<? extends Component>) cls);
+                        if (classStyle != null)
+                            style.setParent(classStyle);
+                        else
+                            throw new UIException("could not resolve parent class style {" + className + "}");
+                    }
+                    else
+                        throw new UIException("class identifier for parent class style {" + className + "} is not assignable to Component");
+                }
+                catch (ClassNotFoundException e) {
+                    throw new UIException("cannot find class for parent class style {" + className + "}", e);
+                }
+            }
+        }
+        // create and return theme
+        return new Theme(nameStyles, classStyles);
+    }
 
     /**
      * A map of styles using a name identifier.
